34ff18650644029c7fdf7b34fa857b5b353f32dd
Steven Frew <steven.fullhouse@gmail.com>
2022-11-11 10:07:22 +0000

    added test files

--- README.md
+++ README.md
@@ -1,46 +1,23 @@
-# Getting Started with Create React App
+# Demo App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1) Run and launch app (localhost:3000)
+2) Scroll to bottom of modal
+3) Open menu on select controls at the foot of the modal
+- Some menus are impossible to get the mouse onto
+- Some menus change size as the mouse moves over the options
 
-## Available Scripts
+In the code, if you change this line:
 
-In the project directory, you can run:
+```typescript
+theme: {(theme) => ({...theme})}
+```
 
-### `npm start`
+to:
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+```typescript
+theme: {(theme) => theme}
+```
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+... (or remove the line entirely) ...
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+... _then the problem goes away!_ 😮
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Button, Modal } from "react-bootstrap"
+import Select from "react-select"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const options = [
+		{ value: "chocolate", label: "Chocolate" },
+		{ value: "strawberry", label: "Strawberry" },
+		{ value: "vanilla", label: "Vanilla" },
+		{ value: "1chocolate", label: "Chocolate" },
+		{ value: "1strawberry", label: "Strawberry" },
+		{ value: "1vanilla", label: "Vanilla" },
+		{ value: "2chocolate", label: "Chocolate" },
+		{ value: "2strawberry", label: "Strawberry" },
+		{ value: "2vanilla", label: "Vanilla" },
+		{ value: "3chocolate", label: "Chocolate" },
+		{ value: "3strawberry", label: "Strawberry" },
+		{ value: "3vanilla", label: "Vanilla" },
+		{ value: "4chocolate", label: "Chocolate" },
+		{ value: "4strawberry", label: "Strawberry" },
+		{ value: "4vanilla", label: "Vanilla" },
+		{ value: "5chocolate", label: "Chocolate" },
+		{ value: "5strawberry", label: "Strawberry" },
+		{ value: "5vanilla", label: "Vanilla" },
+		{ value: "6chocolate", label: "Chocolate" },
+		{ value: "6strawberry", label: "Strawberry" },
+		{ value: "6vanilla", label: "Vanilla" },
+		{ value: "7chocolate", label: "Chocolate" },
+		{ value: "7strawberry", label: "Strawberry" },
+		{ value: "7vanilla", label: "Vanilla" },
+		{ value: "8chocolate", label: "Chocolate" },
+		{ value: "8strawberry", label: "Strawberry" },
+		{ value: "8vanilla", label: "Vanilla" },
+	]
+
+	const items = []
+	for (let index = 0; index < 20; ++index) {
+		items.push(
+			<div
+				key={index}
+				style={{
+					backgroundColor: "white",
+					padding: 10,
+					flex: 1,
+					display: "flex",
+					flexWrap: "nowrap",
+					flexDirection: "row",
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						flex: 0.2,
+					}}
+				>
+					CHOOSE:
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flex: 0.8,
+					}}
+				>
+					<Select
+						menuPosition="fixed"
+						options={options}
+						theme={(theme) => ({ ...theme })}
+						styles={{
+							container: (styles) => ({
+								...styles,
+								display: "flex",
+								flex: 1,
+							}),
+							control: (styles) => ({
+								...styles,
+								display: "flex",
+								flex: 1,
+							}),
+							input: (styles) => {
+								const after = styles["&:after"] as any
+								delete after["content"]
+								styles["&:after"] = after
+								return {
+									...styles,
+									display: "flex",
+								}
+							},
+						}}
+					/>
+				</div>
+			</div>
+		)
+	}
+	return (
+		<div>
+			Hello
+			<Modal show scrollable>
+				<Modal.Header>BLAH</Modal.Header>
+				<Modal.Body>{items}</Modal.Body>
+				<Modal.Header>
+					<Button>OK</Button>
+				</Modal.Header>
+			</Modal>
+		</div>
+	)
 }
 
-export default App;
+export default App
